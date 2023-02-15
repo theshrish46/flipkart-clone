f2365b57b538633ffd56a07ead39f7d8a7be0389
theshrish46 <theshrish46@gmail.com>
2023-02-15 18:15:28 +0530

    worked on header page and categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Categories from './components/Categories'
 import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Categories />
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactSearchBox from 'react-search-box'
 import headerLogo from '../assets/flipkart-icon.png'
+import {IoIosArrowDown, IoIosArrowUp} from 'react-icons/io'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {AiOutlineSearch} from 'react-icons/ai'
+
+
 
 const Header = () => {
     return (
@@ -11,14 +16,26 @@ const Header = () => {
                     <img src={headerLogo} alt='img' className='px-1 w-10 h-8'></img>
                     <span className='px-2 text-white font-bold italic'>Flipkart</span>
                 </div>
-                <div className='w-2/6'>
+
+                <div className='flex justify-center items-center w-1/3'>
+
+                <div className='w-full'>
                     <ReactSearchBox
                         placeholder='Search'
                         value='Groceries'
+                        className='rounded-l-none'
                     />
                 </div>
-                <div>
-                    
+                <div className='text-2xl text-blue-500 p-2 bg-white rounded-r-md'>
+                    <AiOutlineSearch />
+                </div>
+                </div>
+                <div className='text-[1.1em] text-white flex justify-center'>
+                    <button className='mx-4 font-semibold'>Shrish</button>
+                    <button className='mx-4'>Become a seller</button>
+                    <button className='mx-4 flex justify-center items-center'>More <IoIosArrowDown className='mx-1' /> </button>
+                    {/* <IoIosArrowUp /> */}
+                    <button className='mx-4 flex justify-center items-center'>Cart <AiOutlineShoppingCart className='mx-1'/></button>
                 </div>
             </div>
         </div>
