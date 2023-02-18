dd6626cdf07be46b179146049f02171ea394dc4b
theshrish46 <theshrish46@gmail.com>
2023-02-18 23:21:32 +0530

    on 18-2-23

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {appliances} from './../assets/appliances.webp'
+import {fashion} from './../assets/fashion.webp'
+import {electronics} from './../assets/electronics.webp'
+import {home} from './../assets/home.webp'
+import {grocery} from './../assets/grocery.webp'
+import {travel} from './../assets/travel.webp'
+import {twowheelers} from './../assets/twowheelers.webp'
+import {topoffers} from './../assets/topoffers.webp'
+import {mobiles} from './../assets/mobiles.webp'
+import {kids} from './../assets/kids.webp'
 
 const Categories = () => {
   return (
     <div>
       <div>
-
+        <img src='./../assets/appliances.webp' alt='an img'></img>
       </div>
     </div>
   )
