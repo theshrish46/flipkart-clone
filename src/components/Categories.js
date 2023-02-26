b88710376beea87547e59df606be0e5988f3b1a7
theshrish46 <theshrish46@gmail.com>
2023-02-26 18:29:14 +0530

    on 26-2-23

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,13 +1,18 @@
 import React from 'react'
-// import grocery from './../assets/grocery.jpeg'
+import { grocery } from './../assets/grocery.jpeg'
 
 
 const Categories = () => {
   return (
     <div className='w-full h-40'>
       <div className='w-20 h-20'>
-        <img src='./../assets/appliances.jpg' alt='react img' className='w-20 h-20'></img>
-        {/* <img src={grocery} alt='react img2'></img> */}
+        <Image
+          src={grocery}
+          alt='react img'
+          className='w-full h-full px-2'
+          
+        />
+        <span>Appliances</span>
       </div>
     </div>
   )
