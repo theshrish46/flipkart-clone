2bec1adaf71f49ee6891e2c185ee0659a1f8d336
theshrish46 <theshrish46@gmail.com>
2023-03-10 19:19:37 +0530

    worked on categories page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,19 +1,70 @@
-import React from 'react'
-import { grocery } from './../assets/grocery.jpeg'
+import React from 'react';
+import  grocery from './../assets/grocery.png';
+import electronics from './../assets/electronics.png';
+import kids from './../assets/kids.png';
+import fashion from './../assets/fashion.jpeg';
+import home from './../assets/home.jpeg';
+import mobile from './../assets/mobiles.png';
+import travel from './../assets/travel.png';
+import twowheeler from './../assets/twowheelers.png';
+import topoffer from './../assets/topoffers.png';
+import appliances from './../assets/appliances.png';
 
 
 const Categories = () => {
   return (
-    <div className='w-full h-40'>
-      <div className='w-20 h-20'>
-        <Image
-          src={grocery}
-          alt='react img'
-          className='w-full h-full px-2'
-          
-        />
+    <div className='w-full h-28 flex justify-center items-center shadow-md'>
+      
+      <div className='w-32 h-32 px-2 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={grocery} alt="an graphics" className='w-16'/>
+        <span>Grocery</span>
+      </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={mobile} alt="an graphics" className='w-16'/>
+        <span>Mobiles</span>
+      </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={fashion} alt="an graphics" className='w-16'/>
+        <span>Fashion</span>
+      </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={electronics} alt="an graphics" className='w-16'/>
+        <span>Electronics</span>
+      </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={home} alt="an graphics" className='w-16'/>
+        <span>Home</span>
+      </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={appliances} alt="an graphics" className='w-16'/>
         <span>Appliances</span>
       </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={travel} alt="an graphics" className='w-16'/>
+        <span>Travel</span>
+      </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={topoffer} alt="an graphics" className='w-16'/>
+        <span>Top Offers</span>
+      </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={kids} alt="an graphics" className='w-16'/>
+        <span>Kids</span>
+      </div>
+
+      <div className='w-32 h-28 flex flex-col text-[0.9rem] font-semibold justify-center items-center'>
+        <img src={twowheeler} alt="an graphics" className='w-16'/>
+        <span>Two Wheelers</span>
+      </div>
+
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,33 +9,33 @@ import {AiOutlineSearch} from 'react-icons/ai'
 
 const Header = () => {
     return (
-        <div className='w-full h-16 bg-blue-500'>
+        <div className='w-full h-14 bg-blue-500'>
 
             <div className='w-full h-full flex justify-around items-center'>
                 <div className='flex items-center justify-center'>
-                    <img src={headerLogo} alt='img' className='px-1 w-10 h-8'></img>
-                    <span className='px-2 text-white font-bold italic'>Flipkart</span>
+                    <img src={headerLogo} alt='img' className='px-1 w-8 h-6'></img>
+                    <span className='px-2 text-white text-[1.2rem] font-bold italic'>Flipkart</span>
                 </div>
 
                 <div className='flex justify-center items-center w-1/3'>
 
-                <div className='w-full'>
+                <div className='w-full h-10 text-[1rem]'>
                     <ReactSearchBox
                         placeholder='Search'
                         value='Groceries'
-                        className='rounded-l-none'
+                        className='rounded-l-none text-[1rem]'
                     />
                 </div>
                 <div className='text-2xl text-blue-500 p-2 bg-white rounded-r-md'>
                     <AiOutlineSearch />
                 </div>
                 </div>
-                <div className='text-[1.1em] text-white flex justify-center'>
-                    <button className='mx-4 font-semibold'>Shrish</button>
-                    <button className='mx-4'>Become a seller</button>
-                    <button className='mx-4 flex justify-center items-center'>More <IoIosArrowDown className='mx-1' /> </button>
+                <div className='text-[1rem] text-white flex justify-center'>
+                    <button className='mx-4 text-[1rem] font-semibold'>Shrish</button>
+                    <button className='mx-4 text-[1rem]'>Become a seller</button>
+                    <button className='mx-4 text-[1rem] flex justify-center items-center'>More <IoIosArrowDown className='mx-1' /> </button>
                     {/* <IoIosArrowUp /> */}
-                    <button className='mx-4 flex justify-center items-center'>Cart <AiOutlineShoppingCart className='mx-1'/></button>
+                    <button className='mx-4 text-[1rem] flex justify-center items-center'>Cart <AiOutlineShoppingCart className='mx-1 text-[1.1rem]'/></button>
                 </div>
             </div>
         </div>
